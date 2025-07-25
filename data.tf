a9d9e278ee58d2e032951c82ee6f391f438bacf8
Isabelle Sundin <nightstar2023@gmail.com>
2025-07-25 09:48:21 -0400

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,19 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
