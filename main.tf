4def5d11bb576085712977bec78722f95c778c1a
Isabelle Sundin <nightstar2023@gmail.com>
2025-07-25 09:47:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,47 @@
-# main.tf
-provider "aws" {
-  region = var.aws_region
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+resource "aws_s3_bucket" "portfolio_bucket" {
+  bucket = "${var.bucket_prefix}-${random_id.bucket_suffix.hex}"
 
   tags = {
-    Name = "MyEC2Instance"
+    Name        = "Portfolio Bucket"
+    Environment = "Dev"
   }
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
 
   versioning {
     enabled = true
   }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Consider changing to your IP for security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "portfolio_ec2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  security_groups = [aws_security_group.ec2_sg.name]
 
   tags = {
-    Name = "MyS3Bucket"
+    Name = "PortfolioEC2"
   }
 }
