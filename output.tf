63ae1abbae4d3f911c9a12fb37f87900445e6318
Isabelle Sundin <nightstar2023@gmail.com>
2025-07-25 11:11:37 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,7 @@
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.portfolio_bucket.bucket
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
 }
 
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.portfolio_ec2.public_ip
+output "s3_bucket_name" {
+  value = aws_s3_bucket.portfolio_bucket.id
 }
